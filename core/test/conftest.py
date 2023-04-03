efa9d41039a02a11be94aee3965c8d9d20d62a34
HelloSolo <davidalimi148@gmail.com>
2023-04-03 15:07:36 +0100

    moved activate_user fixture to conftest file

--- core/test/conftest.py
+++ core/test/conftest.py
@@ -5,3 +5,13 @@ from rest_framework.test import APIClient
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def activate_user(api_client, create_user):
+    def do_activate_user(data):
+        create_user()
+
+        return api_client.post("/auth/users/activation/", data)
+
+    return do_activate_user
--- core/test/test_user_registration.py
+++ core/test/test_user_registration.py
@@ -18,16 +18,6 @@ def create_user(api_client):
     return do_create_user
 
 
-@pytest.fixture
-def activate_user(api_client, create_user):
-    def do_activate_user(data):
-        create_user()
-
-        return api_client.post("/auth/users/activation/", data)
-
-    return do_activate_user
-
-
 @pytest.mark.django_db
 class TestUserRegistration:
     def test_user_registion_valid_data(self, create_user):
