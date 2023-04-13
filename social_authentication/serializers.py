564322cd570a9c3428a7d4412d92b939e9d47237
HelloSolo <davidalimi148@gmail.com>
2023-04-13 12:09:42 +0100

    added serializers, view & url for auth with google

---
+++ social_authentication/serializers.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+from rest_framework import serializers
+from . import google
+from .register import register_social_user
+from rest_framework.exceptions import AuthenticationFailed
+
+load_dotenv()
+
+
+class GoogleAuthSerializer(serializers.Serializer):
+    credential = serializers.CharField()
+
+    def validate_credential(self, credentials):
+        user_data = google.Google.validate(credentials)  # returns user info from google
+
+        try:
+            user_data["sub"]
+        except:
+            raise serializers.ValidationError(
+                "The token is invalid or expired. Please login again"
+            )
+        if user_data["aud"] != os.environ.get(
+            "CLIENTID"
+        ):  # check if the credential was generated using our app
+            raise AuthenticationFailed(
+                "oops, invalid token or your token cannot authorized by this app "
+            )
+
+        email = user_data["email"]
+        first_name = user_data["given_name"]
+        last_name = user_data["family_name"]
+        auth_provider = "google"
+
+        return register_social_user(
+            provider=auth_provider,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
--- social_authentication/views.py
+++ social_authentication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import GoogleAuthSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class GoogleAuthView(GenericAPIView):
+    serializer_class = GoogleAuthSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = (serializer.validated_data)["credential"]
+        return Response(data, status=status.HTTP_200_OK)
