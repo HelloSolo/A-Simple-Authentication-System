a1cb1ae357212a07d52d9e608a85d81243c4313f
HelloSolo <davidalimi148@gmail.com>
2023-04-13 13:54:45 +0100

    google class now uses credentials & not id_tokens

--- social_authentication/google.py
+++ social_authentication/google.py
@@ -1,23 +1,23 @@
-from google.auth.transport import requests
-from google.oauth2 import id_token
+from google.auth import jwt
 
 
 class Google:
     """Google class to fetch the user's info and return it"""
 
-    @staticmethod  # creates a method that is bound to the class rather than an object of the class
-    def validate(auth_token):
-        """
-        validate method queries the google oauth2 api to fetch the user's info
-        """
+    @staticmethod
+    def validate(credentials):
         try:
-            idinfo = id_token.verify_oauth2_token(
-                auth_token, requests.Request()
-            )  # to verify if the auth_token is issued by google
+            idinfo = jwt.decode(
+                credentials, verify=False
+            )  # decodes the credentials recieved from the google
+
+            # Note To Self: Needs cert to be able to perform google verification
 
             if (
-                "account.google.com" in idinfo["iss"]
-            ):  # checks if the issuer of the token is google
+                "https://accounts.google.com" in idinfo["iss"]
+                and True == idinfo["email_verified"]
+                and "gmail.com" in idinfo["email"]
+            ):  # checks if the issuer of the token is google and the email is verified
                 return idinfo
         except:
             return "The token is either invalid or has expired"
